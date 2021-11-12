02c976e6177f4bf694cc1cf6d86a9754fc49c831
Mateus Fonseca <mateuspaf23@gmail.com>
2021-11-12 09:23:00 -0300

    feat: adds body argument to RemoteAuthentication use case

--- src/data/useCases/authentication/remote-authentication.ts
+++ src/data/useCases/authentication/remote-authentication.ts
@@ -1,11 +1,13 @@
+import { AuthenticationParams } from '../../../domain/useCases/authentication'
 import { HttpPostClient } from '../../protocols/http/http-post-client'
 
 export class RemoteAuthentication {
   constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) { }
 
-  async auth (): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<void> {
     await this.httpPostClient.post({
-      url: this.url
+      url: this.url,
+      body: params
     })
   }
 }
